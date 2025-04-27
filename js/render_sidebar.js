deaa2406d54b0444e46df7f3156dac8349360d89
Gaston Magrassi <gas051093@hotmail.com.ar>
2025-04-27 19:08:35 -0300

    render con js

---
+++ js/render_sidebar.js
@@ -0,0 +1,73 @@
+const sidebar = document.getElementById("sidebar_cont");
+
+const menus = [
+    {
+        nombre : "Inicio",
+        sub_menu: [],
+        ico: "home"
+    },
+    {
+        nombre: "Ordenes",
+        sub_menu: [],
+        ico: "order"
+    },
+    {
+        nombre: "Clientes",
+        sub_menu: [],
+        ico : "client"
+    },
+    {
+        nombre: "Facturacion",
+        sub_menu: ["Alta de Factura","Alta de presupuesto","Nota de credito", "Comprobantes","Metodos de pago"],
+        ico: "billing"
+    },
+    {
+        nombre: "Productos",
+        sub_menu: ["Alta de productos", "Control de stock","categorias"],
+        ico: "product"
+    },
+    {
+        nombre: "Caja",
+        sub_menu: ["Cierre diario", "Archivo de caja","Alta de cajas"],
+        ico: "money"
+    },
+    {
+        nombre: "Configuración",
+        sub_menu: ["Datos de la empresa", "Usuarios", "Preferencias del sistema", "Seguridad"],
+        ico: "config"
+    },
+    {
+        nombre: "Cerrar sesión",
+        sub_menu: [],
+        ico: "lock"
+    }
+]
+
+const render_menu = ()=>{
+    menus.forEach(menu =>{
+        let cont = document.createElement("button");
+        cont.id = `btn_${menu.nombre}`;
+        cont.className = "sidebar__item"
+        if(menu.nombre === "Inicio"){
+            cont.classList.add("sidebar__item--active")
+        }
+        cont.setAttribute("data-target", `content_${menu.nombre}`)
+        cont.innerHTML = (
+            `<img class="sidebar__ico me-3" src="assets/ico/${menu.ico}.svg" alt="">${menu.nombre}`
+        )
+        sidebar.appendChild(cont)
+        if(menu.sub_menu.length){
+            let container = document.createElement("div")
+            container.id = `content_${menu.nombre}`
+            container.className = "sidebar__submenu row justify-items-end m-0";
+                for(const sub of menu.sub_menu){
+                    let cont_sub = document.createElement("button");
+                    cont_sub.className = "sidebar__menu";
+                    cont_sub.innerHTML = (`${sub}`)
+                    container.appendChild(cont_sub);
+            }
+            sidebar.appendChild(container);
+        }
+    } )
+}
+render_menu();
--- js/sidebar_event.js
+++ js/sidebar_event.js
@@ -1,27 +1,43 @@
 function close_menu(){
-    conten_items = document.querySelectorAll(".sidebar__submenu")
+    const conten_items = document.querySelectorAll(".sidebar__submenu");
     conten_items.forEach (item=>{
         item.style.display = "none"
     })
 }
+function disagree_item(){
+    const items = document.querySelectorAll(".sidebar__item");
+    items.forEach(item =>{
+        item.classList.remove("sidebar__item--active");
+    })
+}
 const event_menu = (id_menu) => {
     const menu = document.getElementById(id_menu);
-    if (menu.style.display === "block") {
+    if(!menu){
+        close_menu()
+    } else if (menu.style.display === "block") {
         menu.style.display = "none";
     } else {
         close_menu();
         menu.style.display = "block";
     }
 };
+const item_active = (item_id) =>{
+    const item_selec = document.getElementById(item_id);
+    item_selec.classList.add ("sidebar__item--active");
+};
 
 function item_event(){
-    btn_item = document.querySelectorAll(".sidebar__item")
+    const btn_item = document.querySelectorAll(".sidebar__item")
     btn_item.forEach (button =>{
         button.addEventListener("click", (e) =>{
             const btn_target = e.currentTarget.getAttribute("data-target");
-            event_menu(btn_target);
+            const btn_id = e.currentTarget.id;
+                disagree_item();
+                item_active(btn_id);
+                event_menu(btn_target)
         })
     })
 }
+
 
 item_event();
